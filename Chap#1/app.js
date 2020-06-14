f3198fb2cf05ffa1f0961812c5d7b85bc02a1fb3
syedsaudmasood <syedsaudbinmasood@gmail.com>
2020-06-14 13:15:52 +0500

    commit complete

---
+++ Chap#1/app.js
@@ -0,0 +1,37 @@
+//1. Write a script to greet your website visitor using JS alert
+//box.
+
+alert("HELLO")
+
+// 2. Write a script to display following message on your web
+// page:
+
+alert("Error! Please enter a valid password")
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+alert("Welcome to JS land \n Happy Coding")
+
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to JS land")
+alert("Happy Coding")
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+alert("Hello... I can run JS through my web broswer's console")
+
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.
+
+alert("heya")
+
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
